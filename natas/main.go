493c9f93d91e32cae00ca98f38599d5a2174e7cb
Michael Woolnough <michael.woolnough@gmail.com>
2017-09-04 10:27:46 +0100

    main structure for program and solution to natas0

---
+++ natas/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+
+	"github.com/MJKWoolnough/errors"
+	"github.com/MJKWoolnough/memio"
+)
+
+var buf memio.Buffer
+
+type Grabber interface {
+	Grab(http.Request) (string, error)
+}
+
+type Prefixed struct {
+	Prefix string
+	Length int
+}
+
+func (p Prefixed) Grab(r http.Request) (string, error) {
+	buf = buf[:0]
+	resp, err := http.DefaultClient.Do(&r)
+	if err != nil {
+		return "", err
+	}
+	io.Copy(&buf, resp.Body)
+	resp.Body.Close()
+	index := bytes.Index(buf, []byte(p.Prefix))
+
+	if index < 0 || len(buf) < index+len(p.Prefix)+p.Length {
+		return "", errors.Error("failed to get password")
+	}
+	return string(buf[index+len(p.Prefix) : index+len(p.Prefix)+p.Length]), nil
+}
+
+var levels = [...]Grabber{
+	Prefixed{"The password for natas1 is ", 32},
+}
+
+func main() {
+	var (
+		level    uint
+		password string
+		err      error
+	)
+	flag.UintVar(&level, "l", 0, "level number. > 0 requires password")
+	flag.StringVar(&password, "p", "natas0", "password for initial level")
+	flag.Parse()
+
+	r := http.Request{
+		Header: make(http.Header),
+	}
+
+	for n, grabber := range levels[level:] {
+		n += int(level)
+
+		log.Printf("natas %2d: Solving...", n)
+
+		r.URL, _ = url.Parse(fmt.Sprintf("http://natas%d.natas.labs.overthewire.org/", n))
+		r.SetBasicAuth(fmt.Sprintf("natas%d", n), password)
+
+		password, err = grabber.Grab(r)
+		if err != nil {
+			log.Printf("natas %2d: error: %s", n, err)
+			return
+		}
+		log.Printf("natas %2d: Password: %s", n, password)
+	}
+}
